3e98ccefd92d4a5645f171a581c63bf0e7491dab
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2021-10-28 18:31:14 +0200

    Add SSO CAS

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require "omniauth/strategies/unicaen"
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  OmniAuth.config.logger = Rails.logger
+
+  omniauth_config = Rails.application.secrets.dig(:omniauth)
+
+  if omniauth_config[:cas].present?
+    provider(
+      OmniAuth::Strategies::Unicaen,
+      setup: setup_provider_proc(:cas,
+                                 host: :host
+      )
+    )
+  end
+end
